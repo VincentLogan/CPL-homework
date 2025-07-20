b0ecb7d4c2fa84b522e90ee33747e0bcc71d8b8d
Vincent <1771504139@qq.com>
2025-07-20 23:02:41 +0800

    second

--- 1-types-io/Time.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    char month[20],weekday[20];
-    int year,day,minute,hour,second;
-    scanf("%s%d%d%s%d%d%d",month,&day,&year,weekday,&hour,&minute,&second);
-    printf("%.3s %.3s %d %.2d:%.2d:%.2d %.4d",weekday,month,day,hour,minute,second,year);
-    return 0;
-}
--- 1-types-io/pi.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    double pi,pi_4;
-    pi=log(pow(640320,3)+744)*1.0/sqrt(163);
-    pi_4=4*atan(1.0/5)-atan(1.0/239);
-    printf("%.15f\n%.15f\n",pi,pi_4*4);
-    return 0;
-}
