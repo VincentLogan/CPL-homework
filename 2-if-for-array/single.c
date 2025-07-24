e372cb37b88a4b893637ee3dfb4dbe3a837a0da5
Vincent <1771504139@qq.com>
2025-07-24 23:13:12 +0800

    single初版

---
+++ 2-if-for-array/single.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int one[10005],two[10005];
+    int num;
+    scanf("%d",&num);
+    for(int i=0;i<num*2-1;i++)
+    {
+        scanf("%d",one[i]);
+    }
+    for(int b=0;b<num*2-1;b++)
+    {
+        
+    }
+    printf;
+    return 0;
+}
