66bb1a96d899c22dfc97bbfd354dc674f8e84d09
Vincent <1771504139@qq.com>
2025-07-24 23:39:38 +0800

    single完成出版

--- 2-if-for-array/single.c
+++ 2-if-for-array/single.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int one[10005],two[10005];
-    int num;
+    int one[10005];
+    int num,j;
     scanf("%d",&num);
     for(int i=0;i<num*2-1;i++)
     {
-        scanf("%d",one[i]);
+        scanf("%d",&one[i]);
     }
     for(int b=0;b<num*2-1;b++)
     {
-        
+        for(int a=0;a<=num*2-1;a++)
+        {
+            if(one[b]-one[a]==0&&b!=a)
+            {
+                break;
+            }
+            if(a==num*2-2&&one[b]-one[a]!=0)
+            {
+                printf("%d",one[b]);
+                return 0;
+            }
+        }
     }
-    printf;
-    return 0;
 }
