5322ea043255f0846b5dc1299cac65d498c1034a
Vincent <1771504139@qq.com>
2025-07-29 12:08:11 +0800

    pallindromic第三版（终于过了）

--- homework1/palindromic.c
+++ homework1/palindromic.c
@@ -1,79 +1,114 @@
 #include<stdio.h>
-#include<math.h>
 #include<stdbool.h>
 int main()
 {
-    int digit=1,zhongjianshang,huan;
     bool prime=true;
-    long int left_margin,right_margin,cache=0;
+    int left_margin,right_margin,cache=0,digit_left,digit_right;
     scanf("%d %d",&left_margin,&right_margin);
-    for(int i=left_margin;i<=right_margin;i++)
-    {       
-        if(i%2==0||i%3==0||(i%5==0&&i!=5)||(i%7==0&&i!=7))
-            {
-                continue;
-            }
-        else
+        if(left_margin<=5)
         {
-            cache=0;
-            zhongjianshang=i;
-            prime=true;
-            digit=floor(log10(i))+1;
-            if(digit==1)
+            printf("5\n7\n11\n");
+        }
+        else if(left_margin<=7&&left_margin>5)
+        {
+            printf("7\n11\n");
+        }
+        else if(left_margin<=11&&left_margin>7)
+        {
+            printf("11\n");
+        }
+        for(int w3=1;w3<=9;w3+=2)
+        {
+            for(int w2=0;w2<=9;w2++)
             {
-                if(i==5||i==7)
-                printf("%d\n",i);
-            }
-            else if(digit%2==0)
-            {
-                for(int j=0;j<digit/2;j++)
+                prime=true;
+                cache=w3*100+w2*10+w3;
+                if(cache<left_margin)
                 {
-                    cache=cache*10+zhongjianshang%10;
-                    zhongjianshang=zhongjianshang/10;
+                    continue;
                 }
-                if(zhongjianshang==cache)
+                if(cache>right_margin)
                 {
-                    for(int a=3;a*a<=i;a+=2)
+                    return 0;
+                }
+                for(int j=2;j*j<=cache;j++)
+                {
+                    if(cache%j==0)
                     {
-                        if(i%a==0)
+                        prime=false;
+                        break;
+                    }
+                }
+                if(prime)
+                {
+                    printf("%d\n",cache);
+                }
+            }
+        }
+        for(int w5=1;w5<=9;w5+=2)
+        {
+            for(int w3=0;w3<=9;w3++)
+            {
+                for(int w1=0;w1<=9;w1++)
+                {
+                    prime=true;
+                    cache=w5*10001+w3*1010+w1*100;
+                    if(cache<left_margin)
+                    {
+                        continue;
+                    }
+                    if(cache>right_margin)
+                    {
+                        return 0;
+                    }
+                for(int j=2;j*j<=cache;j++)
+                {
+                    if(cache%j==0)
+                    {
+                        prime=false;
+                        break;
+                    }
+                }
+                if(prime)
+                {
+                    printf("%d\n",cache);
+                }
+                }
+            }
+        }
+        for(int w7=1;w7<=9;w7+=2)
+        {
+            for(int w5=0;w5<=9;w5++)
+            {
+                for(int w3=0;w3<=9;w3++)
+                {
+                    for(int w1=0;w1<=9;w1++)
+                    {
+                        prime=true;
+                        cache=w7*1000001+w5*100010+w3*10100+w1*1000;
+                        if(cache<left_margin)
                         {
-                            prime=false;
-                            break;
+                            continue;
+                        }
+                        if(cache>right_margin)
+                        {
+                            return 0;
+                        }
+                        for(int j=2;j*j<=cache;j++)
+                        {
+                            if(cache%j==0)
+                            {
+                                prime=false;
+                                break;
+                            }
+                        }
+                        if(prime)
+                        {
+                            printf("%d\n",cache);
                         }
                     }
-                    if(prime)
-                    {
-                    printf("%d\n",i);
-                    continue;
-                    }
                 }
             }
-            else
-            {
-                for(int j=0;j<(digit-1)/2;j++)
-                {
-                    cache=cache*10+zhongjianshang%10;
-                    zhongjianshang=zhongjianshang/10;
-                }
-                if(zhongjianshang/10==cache)
-                {
-                    for(int a=3;a*a<=i;a+=2)
-                    {
-                        if(i%a==0)
-                        {
-                            prime=false;
-                            break;
-                        }
-                    }
-                    if(prime)
-                    {
-                    printf("%d\n",i);
-                    continue;
-                    }
-                }
-            }
-        }    
-        
-    }
+        }
     return 0;
 }
