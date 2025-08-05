a35b4271014c4ae8c4364386fc6c8ce8f9a628de
Vincent <1771504139@qq.com>
2025-08-05 18:26:49 +0800

    Marathon第一版（非文件格式输入）

---
+++ homework2/Marathon.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,skip_point=1,save=0,S1,S2,sum=0;
+    scanf("%d",&n);
+    int coordinate[n][2];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d%d",&coordinate[i][0],&coordinate[i][1]);
+    }
+    for(int i=1;i<n-1;i++)
+    {
+        S1=abs(coordinate[i-1][0]-coordinate[i+1][0])+abs(coordinate[i-1][1]-coordinate[i+1][1]);
+        S2=abs(coordinate[i][0]-coordinate[i-1][0])+abs(coordinate[i][1]-coordinate[i-1][1])+abs(coordinate[i][0]-coordinate[i+1][0])+abs(coordinate[i][1]-coordinate[i+1][1]);;
+        if(save<S2-S1)
+        {
+            save=S2-S1;
+            skip_point=i;
+        }
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        sum+=abs(coordinate[i][0]-coordinate[i+1][0])+abs(coordinate[i][1]-coordinate[i+1][1]);
+    }
+    printf("%d",sum-save);
+    return 0;
+}
